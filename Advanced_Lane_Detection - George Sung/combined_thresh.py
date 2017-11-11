acf14eec28effd24c1e7d2e2c856895a1b3c482b
DonatoK <donatokava@gmail.com>
2017-11-10 19:26:22 -0500

    Add files via upload

--- Advanced_Lane_Detection - George Sung/combined_thresh.py
+++ Advanced_Lane_Detection - George Sung/combined_thresh.py
@@ -75,7 +75,7 @@ def hls_thresh(img, thresh=(100, 255)):
 	Convert RGB to HLS and threshold to binary image using S channel
 	"""
 	hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-	s_channel = hls[:,:,2]
+	s_channel = hls[:,:,1]
 	binary_output = np.zeros_like(s_channel)
 	binary_output[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 1
 	return binary_output
